1dafe232f2cff3ff9bdab00828800f7130370afb
Simona <62394338+missim2020@users.noreply.github.com>
2021-02-06 12:51:23 +0000

    create Series Table and Route; create issue Table and Route

--- api/api.js
+++ api/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 const artistRouter = require('./artist');
+const seriesRouter = require('./series.js');
 
 apiRouter.use('/artist', artistRouter);
+apiRouter.use('/series', seriesRouter);
 
 
 module.exports=apiRouter;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const apiRouter = require('./api/api'); 
 
 const app= express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
